6629646039eec31fb6613b76c0dd59611b324a3e
Andrew Krizhanovsky <andrew.krizhanovsky@gmail.com>
2017-03-22 16:53:25 +0300

    +auth

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+@section('title')
+Wcorpus
+@endsection
+
+@section('content')
+            <div class="panel panel-default">
+                <div class="panel-heading">About project</div>
+
+                <div class="panel-body"></div>
+            </div>
+@endsection
